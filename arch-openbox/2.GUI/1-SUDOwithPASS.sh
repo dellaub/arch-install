a38360bbf32d638efcd2f7ac36ed3323679eea7b
dr0x <61434992+dellaub@users.noreply.github.com>
2020-04-26 17:46:41 +0200

    Update 1-SUDOwithPASS.sh

--- arch-openbox/2.GUI/1-SUDOwithPASS.sh
+++ arch-openbox/2.GUI/1-SUDOwithPASS.sh
@@ -13,14 +13,14 @@ set -e
 		echo -e "\033[1;37m# [ SUDO PERMISSIONS WITHOUT PASSWORD ] #\033[0m "
 
 		# enable sudo with password to the wheel group user	
-		sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers > /etc/sudoers.tmp
+		sudo sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers > /etc/sudoers.tmp
 		
 		# disable sudo without password to the wheel group user
-		sed 's/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers.tmp > /etc/sudoers.tmp2
-		mv /etc/sudoers.tmp2 /etc/sudoers
-		rm /etc/sudoers.tmp
+		sudo sed 's/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers.tmp > /etc/sudoers.tmp2
+		sudo mv /etc/sudoers.tmp2 /etc/sudoers
+		sudo rm /etc/sudoers.tmp
 		sleep 2
-		cat /etc/sudoers | grep wheel
+		sudo cat /etc/sudoers | grep wheel
 
 		# finish
 		echo -e " \n\033[1;37m\033[41m[ !! INSTALLATION FINISHED !! ]\033[0m \n"
